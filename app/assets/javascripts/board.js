33aeaca9d408b1c81301b7fda11e8db99ae83673
Parker Lawrence <parker.alford@gmail.com>
2015-06-02 16:20:14 -0400

    merge sample selector for switching samples

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,65 +1,126 @@
 $(document).ready(function() {
-  // Set audio context
-  var context = new AudioContext();
+    // AUDIO SETUP
+    // Set audio context
+    var context = new AudioContext();
 
-  // Define function to load audio associated with a pad html object
-  function loadAudio(object, url) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function() {
-      context.decodeAudioData(request.response, function(buffer) {
-        object.buffer = buffer;
-      });
+    // Define function to load audio associated with a pad html object
+    function loadAudio(object, url) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer';
+        request.onload = function() {
+            context.decodeAudioData(request.response, function(buffer) {
+                object.buffer = buffer;
+            });
+        }
+        request.send();
     }
-    request.send();
-  }
 
-  // Define function to add audio properties to a pad
-  function addAudioProperties(object) {
-    object.name = object.id;
-    object.source = $(object).data('url');
-    loadAudio(object, object.source);
-    object.play = function() {
-      var s = context.createBufferSource();
-      s.buffer = object.buffer;
-      s.connect(context.destination);
-      s.start(0);
-      object.s = s;
+    // Define function to add audio properties to a pad
+    function addAudioProperties(object, url) {
+        object.name = object.id;
+        object.source = url || $(object).data('url');
+        loadAudio(object, object.source);
+        object.play = function() {
+            var s = context.createBufferSource();
+            s.buffer = object.buffer;
+            s.connect(context.destination);
+            s.start(0);
+            object.s = s;
+        }
     }
-  }
 
-  // Add audio properties for each pad
-  $('.pad').each(function() {
-    addAudioProperties(this);
-  });
+    // Add audio properties for each pad
+    $('.pad').each(function() {
+        addAudioProperties(this);
+    });
 
-  // Define key index
-  var keys = [84,89,85,71,72,74,66,78,77];
+    // Define key index
+    var keys = [84,89,85,71,72,74,66,78,77];
 
-  // Add button press
-  addEventListener("keydown", function(event) {
-    for (var i = 0; i < 9; i++) {
-      if (event.keyCode == keys[i]) {
-        var pad_id = '#pad-' + i;
-        $(pad_id)[0].play();
-        $(pad_id).css("background-color","blue");
-      };
-    };
-  });
-  addEventListener("keyup", function(event) {
-    for (var i = 0; i < 9; i++) {
-      if (event.keyCode == keys[i]) {
-        var pad_id = '#pad-' + i;
-        $(pad_id).css("background-color","black");
-      };
-    };
-  });
+    // Add button press
+    addEventListener("keydown", function(event) {
+        for (var i = 0; i < 9; i++) {
+            if (event.keyCode == keys[i]) {
+                var pad_id = '#pad-' + i;
+                $(pad_id)[0].play();
+                $(pad_id).css("background-color","blue");
+            };
+        };
+    });
+    addEventListener("keyup", function(event) {
+        for (var i = 0; i < 9; i++) {
+            if (event.keyCode == keys[i]) {
+                var pad_id = '#pad-' + i;
+                $(pad_id).css("background-color","black");
+            };
+        };
+    });
 
-  $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-      $("#menu-arrow").toggleClass("glyphicon-chevron-right glyphicon-chevron-left");
-  });
+    // CLICK TRIGGERS
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        $("#menu-arrow").toggleClass("glyphicon-chevron-right glyphicon-chevron-left");
+    });
 
+    // fun w/ global variables
+    var padId;
+
+    $('#change-pad').click(function(){
+        // Trigger pop-up directing user to click the pad they want to change
+        // Add class to pads that makes them highlight on mouseover
+        $('.pad').addClass('selectable');
+        $('.selectable').click(function(){
+            $('#sampleModal').modal('show');
+            padId = this.id;
+        });
+        alert("click a pad to change");
+    });
+
+    // AJAX REQUEST
+    function SampleLiBuilder(sample_json){
+        var colors = ["blue","green","red","orange","yellow","purple","white","grey","black"]
+        var htmlString = '<li class="sample-list" style="color:'
+            +colors[Math.floor(Math.random()*colors.length)]
+            +'" data-url="'
+            + sample_json.url
+            +'"><p>'
+            +sample_json.name
+            +'</p></li>'
+        return htmlString
+    }
+
+    $.getJSON( "/samples.json", function( data ) {
+        data.forEach(function(sample_json){
+
+            var htmlElement = $(SampleLiBuilder(sample_json))
+
+            $('.modal-body ul').append(htmlElement[0])
+
+            addAudioProperties(htmlElement[0]);
+
+            $(htmlElement[0]).click(function(){
+                this.play();
+                //remove active class from siblings
+                $(this).siblings().removeClass("active");
+                //add active class to clicked item
+                $(this).addClass('active');
+            });
+        });
+
+        $("#confirm-sample").click(function(){
+            var $sample_element = $('li.active');
+            var sample_url = $sample_element.data("url");
+            var sample_name = $sample_element.text();
+            $('#' + padId).attr("data-url",sample_url);
+            addAudioProperties($('#' + padId)[0], sample_url);
+        });
+
+        // General cancel
+        $("#sampleModal").on('hidden.bs.modal', function(){
+            $('.selectable').removeClass('selectable');
+            $('.sample-list').removeClass('active');
+        });
+    });
 });
