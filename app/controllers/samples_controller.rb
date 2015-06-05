4edeb0c8db2a007f5331b31b3a15922d76cbf99e
sephhh <sephkramer@gmail.com>
2015-06-05 11:33:49 -0400

    add new sample page

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -8,4 +8,10 @@ class SamplesController < ApplicationController
     end 
   end
 
+  def new
+    unless user_signed_in?
+      redirect_to new_user_session_path, :alert => "You must be logged in to create a sample."
+    end
+  end
+
 end
