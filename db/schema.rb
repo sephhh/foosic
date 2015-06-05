49fbb1e89126ae1ef83e993927148d59811c93dd
sephhh <sephkramer@gmail.com>
2015-06-05 11:52:58 -0400

    add migration for dropbox token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605140606) do
+ActiveRecord::Schema.define(version: 20150605154601) do
 
   create_table "boards", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150605140606) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "dropbox_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
